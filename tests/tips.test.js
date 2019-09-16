cfedf424286770ae1a9539c5307389451b202b14
Jack Dwyer <jdwy215@gmail.com>
2019-09-16 18:04:21 -0400

    adding tip test

---
+++ tests/tips.test.js
@@ -0,0 +1,13 @@
+import Foursquare from "../Foursquare";
+
+describe("tips", () => {
+  // Setup Keys
+  require("dotenv").config();
+  const { CLIENT_ID, CLIENT_SECRET } = process.env;
+  const foursquare = new Foursquare(CLIENT_ID, CLIENT_SECRET);
+
+  test("should return details for a specific tip id", async () => {
+    const data = await foursquare.tips.getTip("53bc46b7498e355aed38c696");
+    expect(data.response.tip).toBeTruthy();
+  });
+});
