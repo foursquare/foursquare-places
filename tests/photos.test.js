aa2c9a75dc6fe14062e65df4073a0ecb6e4ea4d7
Jack Dwyer <jdwy215@gmail.com>
2019-09-16 18:04:46 -0400

    updating photo method

--- tests/photos.test.js
+++ tests/photos.test.js
@@ -7,7 +7,7 @@ describe("venues", () => {
   const foursquare = new Foursquare(CLIENT_ID, CLIENT_SECRET);
 
   test("should return photo details", async () => {
-    const data = await foursquare.photos.details("5d49f1c7d0cd2100086be853");
+    const data = await foursquare.photos.getPhoto("5d49f1c7d0cd2100086be853");
     expect(data.response.photo).toBeTruthy();
   });
 });
